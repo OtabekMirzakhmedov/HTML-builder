764e99c3bbc4df77a61f2e282fcb8ce639f7fccf
EVISION\Otabek.Mirzakhmedov <otabek.mirzakhmedov@wolterskluwer.com>
2024-01-24 12:35:47 +0500

    fix: removing syncs

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,145 +2,170 @@ const fs = require('fs');
 const path = require('path');
 
 function buildPage() {
-  const srcComponentsDir = path.join(__dirname, 'components');
-  const srcStylesDir = path.join(__dirname, 'styles');
-  const srcAssetsDir = path.join(__dirname, 'assets');
-  const destDir = path.join(__dirname, 'project-dist');
+  const projectDistDir = path.join(__dirname, 'project-dist');
+  const assetsDir = path.join(projectDistDir, 'assets');
+  const indexPath = path.join(projectDistDir, 'index.html');
+  const stylePath = path.join(projectDistDir, 'style.css');
 
-  fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (templateErr, templateContent) => {
-    if (templateErr) {
-      console.error('Error reading template.html:', templateErr.message);
+  fs.mkdir(projectDistDir, { recursive: true }, (err) => {
+    if (err) {
+      console.error('Error creating project-dist directory:', err);
       process.exit(1);
     }
+    console.log('Project-dist directory created.');
 
-    fs.mkdir(destDir, { recursive: true }, (mkdirErr) => {
-      if (mkdirErr) {
-        console.error('Error creating project-dist folder:', mkdirErr.message);
+    fs.mkdir(assetsDir, { recursive: true }, (err) => {
+      if (err) {
+        console.error('Error creating assets directory:', err);
         process.exit(1);
       }
+      console.log('Assets directory created.');
 
-      const indexContent = templateContent.replace(/{{(.*?)}}/g, (match, componentName) => {
-        const componentPath = path.join(srcComponentsDir, componentName + '.html');
-
-        try {
-          const componentContent = fs.readFileSync(componentPath, 'utf-8');
-          return componentContent;
-        } catch (componentErr) {
-          console.error(`Error: Invalid template tag found: ${match}`);
+      fs.open(indexPath, 'w', (err) => {
+        if (err) {
+          console.error('Error creating index.html:', err);
           process.exit(1);
         }
-      });
+        console.log('Index.html file created.');
 
-      fs.writeFile(path.join(destDir, 'index.html'), indexContent, (writeIndexErr) => {
-        if (writeIndexErr) {
-          console.error('Error writing index.html:', writeIndexErr.message);
-          process.exit(1);
-        }
-
-        fs.readdir(srcStylesDir, (readStylesErr, styleFiles) => {
-          if (readStylesErr) {
-            console.error('Error reading styles directory:', readStylesErr.message);
+        let contentTemplate = '';
+        fs.readFile(path.join(__dirname, 'template.html'), (err, data) => {
+          if (err) {
+            console.error('Error reading template.html:', err);
             process.exit(1);
           }
+          contentTemplate = data.toString();
 
-          const stylesContentArray = [];
+          fs.readdir(path.join(__dirname, 'components'), { withFileTypes: true }, (err, components) => {
+            if (err) {
+              console.error('Error reading components directory:', err);
+              process.exit(1);
+            }
 
-          const processStyleFile = (index) => {
-            if (index < styleFiles.length) {
-              const styleFile = styleFiles[index];
+            let componentsProcessed = 0;
 
-              if (path.extname(styleFile).toLowerCase() === '.css') {
-                const stylePath = path.join(srcStylesDir, styleFile);
-
-                fs.readFile(stylePath, 'utf-8', (readStyleErr, styleContent) => {
-                  if (readStyleErr) {
-                    console.error(`Error reading style file ${styleFile}:`, readStyleErr.message);
-                  } else {
-                    stylesContentArray.push(styleContent);
+            components.forEach((element) => {
+              if (element.isFile()) {
+                const tempComponent = element.name.slice(0, element.name.indexOf('.'));
+                fs.readFile(path.join(__dirname, 'components', element.name), (err, data) => {
+                  if (err) {
+                    console.error(`Error reading component ${element.name}:`, err);
+                    process.exit(1);
                   }
+                  contentTemplate = contentTemplate.replace(`{{${tempComponent}}}`, data.toString());
 
-                  processStyleFile(index + 1);
-                });
-              } else {
-                processStyleFile(index + 1);
-              }
-            } else {
-              const stylesContent = stylesContentArray.join('\n');
-
-              fs.writeFile(path.join(destDir, 'style.css'), stylesContent, (writeStylesErr) => {
-                if (writeStylesErr) {
-                  console.error('Error writing style.css:', writeStylesErr.message);
-                  process.exit(1);
-                }
-
-                const destAssetsDir = path.join(destDir, 'assets');
-
-                const copyAssets = (src, dest, callback) => {
-                  fs.mkdir(dest, { recursive: true }, (mkdirErr) => {
-                    if (mkdirErr) {
-                      console.error('Error creating destination assets folder:', mkdirErr.message);
-                      process.exit(1);
-                    }
-
-                    fs.readdir(src, (readAssetsErr, files) => {
-                      if (readAssetsErr) {
-                        console.error('Error reading assets directory:', readAssetsErr.message);
+                  componentsProcessed++;
+                  if (componentsProcessed === components.length) {
+                    fs.writeFile(indexPath, contentTemplate, (err) => {
+                      if (err) {
+                        console.error('Error writing index.html:', err);
                         process.exit(1);
                       }
-
-                      let count = files.length;
-
-                      if (count === 0) {
-                        callback();
-                      }
-
-                      const decrementAndCheck = () => {
-                        count--;
-                        if (count === 0) {
-                          callback();
-                        }
-                      };
-
-                      files.forEach((file) => {
-                        const srcPath = path.join(src, file);
-                        const destPath = path.join(dest, file);
-
-                        fs.stat(srcPath, (statErr, stats) => {
-                          if (statErr) {
-                            console.error(`Error getting stats for ${srcPath}:`, statErr.message);
-                            process.exit(1);
-                          }
-
-                          if (stats.isDirectory()) {
-                            copyAssets(srcPath, destPath, decrementAndCheck);
-                          } else {
-                            fs.copyFile(srcPath, destPath, (copyFileErr) => {
-                              if (copyFileErr) {
-                                console.error(`Error copying file ${file}:`, copyFileErr.message);
-                                process.exit(1);
-                              }
-
-                              decrementAndCheck();
-                            });
-                          }
-                        });
-                      });
+                      console.log('Index.html content updated.');
+                      createStyleFile();
                     });
-                  });
-                };
-
-                copyAssets(srcAssetsDir, destAssetsDir, () => {
-                  console.log('Build completed.');
+                  }
                 });
-              });
-            }
-          };
-
-          processStyleFile(0);
+              } else {
+                componentsProcessed++;
+                if (componentsProcessed === components.length) {
+                  createStyleFile();
+                }
+              }
+            });
+          });
         });
       });
     });
   });
+
+  function createStyleFile() {
+    fs.open(stylePath, 'w', (err) => {
+      if (err) {
+        console.error('Error creating style.css:', err);
+        process.exit(1);
+      }
+      console.log('Style.css file created.');
+
+      fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.error('Error reading styles directory:', err);
+          process.exit(1);
+        }
+
+        files.forEach((file) => {
+          if (file.isFile() && path.extname(file.name) === '.css') {
+            fs.readFile(path.join(__dirname, 'styles', file.name), (err, data) => {
+              if (err) {
+                console.error(`Error reading style file ${file.name}:`, err);
+                process.exit(1);
+              }
+
+              fs.appendFile(stylePath, data, (err) => {
+                if (err) {
+                  console.error('Error appending to style.css:', err);
+                  process.exit(1);
+                }
+              });
+            });
+          }
+        });
+
+        console.log('Style.css content updated.');
+
+        copyAssets();
+      });
+    });
+  }
+
+  function copyAssets() {
+    fs.readdir(path.join(__dirname, 'assets'), (error, files) => {
+      if (error) {
+        console.error('Error reading assets directory:', error);
+        process.exit(1);
+      }
+      console.log('Assets directory cleared.');
+
+      let assetsProcessed = 0;
+
+      files.forEach((file) => {
+        const destFolder = path.join(assetsDir, file);
+
+        fs.mkdir(destFolder, { recursive: true }, (err) => {
+          if (err) {
+            console.error(`Error creating destination folder ${destFolder}:`, err);
+            process.exit(1);
+          }
+          console.log(`Destination folder ${destFolder} created.`);
+
+          fs.readdir(path.join(__dirname, 'assets', file), (err, content) => {
+            if (err) {
+              console.error(`Error reading assets subfolder ${file}:`, err);
+              process.exit(1);
+            }
+
+            content.forEach((item) => {
+              const srcPath = path.join(__dirname, 'assets', file, item);
+              const destPath = path.join(destFolder, item);
+
+              fs.copyFile(srcPath, destPath, (err) => {
+                if (err) {
+                  console.error(`Error copying file ${item}:`, err);
+                  process.exit(1);
+                }
+
+                assetsProcessed++;
+                if (assetsProcessed === files.length) {
+                  console.log('Assets copied successfully.');
+                  console.log('Build completed.');
+                }
+              });
+            });
+          });
+        });
+      });
+    });
+  }
 }
 
 buildPage();
